1287d1e9a6cdf5ef89644d9d22ccfccab550a3e4
listingslab <listingslab@gmail.com>
2019-12-04 05:58:58 +1100

    Refactory ()

--- python/README.md
+++
@@ -1,5 +0,0 @@
-## Python
-
-- [Camera](camera.py)
-
-[UP](../)
--- react/src/ReactRouter.jsx
+++ react/src/ReactRouter.jsx
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import cn from 'classnames';
