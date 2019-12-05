f014c7255066d1013c7188fd618113165373dd11
listingslab <listingslab@gmail.com>
2019-12-06 06:42:24 +1000

    New build

---
+++ node/build/precache-manifest.4517d05c8d648eedcdce6fe68d050ea6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c512a5df173b50e79d02a461d40895d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b323c61b5218fc99f7a",
+    "url": "/static/css/2.d9a8a5a7.chunk.css"
+  },
+  {
+    "revision": "5b323c61b5218fc99f7a",
+    "url": "/static/js/2.cdc96ff2.chunk.js"
+  },
+  {
+    "revision": "e81b793b701f3ff06318",
+    "url": "/static/js/main.ba21a01b.chunk.js"
+  },
+  {
+    "revision": "a40d6db6668b4b9c872e",
+    "url": "/static/js/runtime-main.b718bb78.js"
+  }
+]);
---
+++ react/public/README.md
@@ -0,0 +1,5 @@
+## Iconduit
+
+We have used an excellent, fledgling build system called [Iconduit](https://github.com/iconduit) to create a pixel perfect PWA with all the trimmings.
+
+[Find out more](https://docs.listingslab.com/docs/examples/iconduit/)
