72987486f64d72cf2c2c2b928005fcc66289cb6a
listingslab <listingslab@gmail.com>
2019-12-10 12:33:04 +1000

    Constrain width to 1200px

--- react/src/theme/App.Style.jsx
+++ react/src/theme/App.Style.jsx
@@ -3,7 +3,8 @@ import commonStyles from "./commonStyles";
 export const styles = theme => ({
     ...commonStyles(theme),
     view: {
-        margin: 'auto'
+        margin: 'auto',
+        maxWidth: 1200,
     },
     pad: {
         // margin: theme.spacing(),
