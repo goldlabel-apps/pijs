8992ef6202fffffa8b2893c1b65278bfeaf383d7
listingslab <listingslab@gmail.com>
2019-12-12 15:42:13 +1000

    Add mui single theme file

---
+++ react/src/theme/mui.jsx
@@ -0,0 +1,39 @@
+
+const theme = {
+    palette: {
+        "type": "light",
+        primary: {
+            main: 'rgb(255,102,0)',
+        },
+        secondary: {
+            main: '#FFFFFF',
+        },
+        background: {
+            default: "#126970",
+            paper: "rgba(255, 255, 255, 1)",
+            appBar: "#f5f5f5",
+            contentFrame: "#eeeeee"
+        },
+        text: {
+            primary: "rgba(0, 0, 0, 0.87)",
+            secondary: "rgba(0, 0, 0, 0.54)",
+            disabled: "rgba(0, 0, 0, 0.38)",
+            hint: "rgba(0, 0, 0, 0.38)",
+            icon: "rgba(0, 0, 0, 0.38)",
+            divider: "rgba(0, 0, 0, 0.12)",
+            lightDivider: "rgba(0, 0, 0, 0.075)",
+        },
+        input: {
+            "helperText": "rgba(0, 0, 0, 0.54)",
+            "labelText": "rgba(0, 0, 0, 0.54)",
+            "inputText": "rgba(0, 0, 0, 0.87)",
+            "disabled": "rgba(0, 0, 0, 0.42)"
+        },
+        action: {
+            "active": "rgba(0, 0, 0, 0.54)",
+            "disabled": "rgba(0, 0, 0, 0.26)"
+        },
+    }
+}
+
+export default theme;
