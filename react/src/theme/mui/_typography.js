784a9e4c1e523963e175cf069636b67421403011
noobie2019 <noobie2019@listingslab.com>
2019-12-14 05:44:29 +1000

    Refeactors mui theme implementation

--- react/src/theme/mui/_typography.js
+++
@@ -1,7 +0,0 @@
-
-const typography = {
-  "useNextVariants": true,
-  "fontFamily": "\"Helvetica\", sans-serif",
-}
-
-export default typography;
--- react/src/theme/mui/palette/_action.js
+++
@@ -1,8 +0,0 @@
-
-
-const action = {
-  "active": "rgba(0, 0, 0, 0.54)",
-  "disabled": "rgba(0, 0, 0, 0.26)"
-}
-
-export default action;
