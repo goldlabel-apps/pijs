01013ecd7bc4eed2209dc3cdcdd99299b9d59e26
listingslab <listingslab@gmail.com>
2019-12-14 14:38:49 +1000

    Deploy process outline

--- bin/pi_deploy.sh
+++ bin/pi_deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# This is what the deploy process is currently.
+# Right now it's a boring and silly manual process
 
 # Build react `yarn build`
 # Rename index.html to react.html (long story)
+# mv /react/build to /node/build
+# commit to git and push
+# ssh into the pi
+# cd to the correct directory
+# git pull
