1dab9d9b0a8945e65334b35a98c95ecbe7b76066
listingslab <listingslab@gmail.com>
2019-12-20 07:51:57 +1000

    Bottom AppBar

--- react/src/components/index.js
+++ react/src/components/index.js
@@ -1,17 +1,11 @@
 import Icon from "./Icon";
 import Nav from "./Nav";
+import PiPlayer from "./PiPlayer";
+
 import Home from "./screens/Home";
 import Map from "./screens/Map";
 import NotFound from "./screens/NotFound";
 import Weather from "./screens/Weather";
 import Webcam from "./screens/Webcam";
 
-export {
-  Home,
-  Icon,
-  Nav,
-  Map,
-  NotFound,
-  Weather,
-  Webcam,
-};
+export { Home, Icon, Nav, PiPlayer, Map, NotFound, Weather, Webcam };
--- react/src/theme/AppShell.Style.jsx
+++ react/src/theme/AppShell.Style.jsx
@@ -4,9 +4,40 @@ import commonStyles from "./commonStyles";
 export const styles = theme => ({
     ...commonStyles(theme),
     appShell: {
-        margin: 'auto',
-        maxWidth: 800,
     },
+    piPlayer: {
+        border: '1px solid #a89a2c',
+    },
+
+    text: {
+        padding: theme.spacing(2, 2, 0),
+    },
+    paper: {
+        paddingBottom: 50,
+    },
+    list: {
+        marginBottom: theme.spacing(2),
+    },
+    subheader: {
+        backgroundColor: theme.palette.background.paper,
+    },
+    appBar: {
+        top: 'auto',
+        bottom: 0,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    fabButton: {
+        position: 'absolute',
+        zIndex: 1,
+        top: -30,
+        left: 0,
+        right: 0,
+        margin: '0 auto',
+    },
+
+
     navCallout: {
         // margin: theme.spacing(),
     },
