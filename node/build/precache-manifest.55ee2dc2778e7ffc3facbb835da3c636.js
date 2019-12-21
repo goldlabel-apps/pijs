2afdacbb47e44175905ae50b92b290fab3e0b64a
listingslab <listingslab@gmail.com>
2019-12-22 04:49:42 +1000

    build 0.4.1

--- node/build/precache-manifest.55ee2dc2778e7ffc3facbb835da3c636.js
+++ node/build/precache-manifest.55ee2dc2778e7ffc3facbb835da3c636.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f90671b2c6ab3b82820d836283281f20",
+    "revision": "aeca6cf0cede982243783b193a329ea9",
     "url": "/index.html"
   },
   {
-    "revision": "17d68bf665e4103233a9",
+    "revision": "3a2056cc8eeee2719e98",
     "url": "/static/css/main.e3daf8d4.chunk.css"
   },
   {
-    "revision": "d6038838ab630b392268",
-    "url": "/static/js/2.12df88d8.chunk.js"
+    "revision": "b5ac74c76d27190993bd",
+    "url": "/static/js/2.9e4bad85.chunk.js"
   },
   {
-    "revision": "17d68bf665e4103233a9",
-    "url": "/static/js/main.c711a13a.chunk.js"
+    "revision": "3a2056cc8eeee2719e98",
+    "url": "/static/js/main.c145df25.chunk.js"
   },
   {
     "revision": "8b28100c1706a5ba1b40",
