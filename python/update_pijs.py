a6f0e55f8ac36165ad6a450573241a746f3179a8
listingslab <listingslab@gmail.com>
2019-12-29 07:48:58 +1000

    color

--- python/update_pijs.py
+++ python/update_pijs.py
@@ -30,7 +30,7 @@ with open('./node/build/data/pijs.json', 'w') as outfile:
 camera = PiCamera()
 
 camera.resolution = (800, 450)
-camera.annotate_background = Color('rgba(0,0,0,0.5)')
+camera.annotate_background = Color('#333333')
 camera.annotate_text = str(round(unix_epoch))
 camera.annotate_text_size = 35
 camera.capture('./node/build/jpg/current-photo.jpg')
