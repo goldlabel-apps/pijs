411ec5382eb35a9f834345e2b5751f1aafab207f
listingslab <listingslab@gmail.com>
2019-12-28 18:08:23 +1000

    unix epoch

--- python/save_envirophat.py
+++ python/save_envirophat.py
@@ -1,15 +1,18 @@
 import json
-import datetime
+from datetime import datetime
+from time import mktime
+
+dt = datetime.now()
+sec_since_epoch = mktime(dt.timetuple()) + dt.microsecond/1000000.0
+unix_epoch = sec_since_epoch * 1000
 
 data = {}
 data['environPhat'] = []
-data['environPhat'].append(
-    {
-        'time': datetime.timestamp()
-    }
-)
+data['environPhat'].append({
+    'time': unix_epoch
+})
 
 with open('./node/build/data/envirophat.json', 'w') as outfile:
     json.dump(data, outfile)
 
-print("envirophat.json saved")
+print("New File Saved https://pi.listingslab.io/data/envirophat.json")
