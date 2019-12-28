6d29565917a26d33a37b528e69f49fa7030cdda7
listingslab <listingslab@gmail.com>
2019-12-28 18:03:36 +1000

    timestamp

--- python/save_envirophat.py
+++ python/save_envirophat.py
@@ -3,7 +3,11 @@ import datetime
 
 data = {}
 data['environPhat'] = []
-data['environPhat'].append({'time': datetime.datetime.now().isoformat()})
+data['environPhat'].append(
+    {
+        'time': datetime.timestamp()
+    }
+)
 
 with open('./node/build/data/envirophat.json', 'w') as outfile:
     json.dump(data, outfile)
