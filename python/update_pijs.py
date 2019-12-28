1c20136574414e6aabc86e4f62cab2be9b18377a
listingslab <listingslab@gmail.com>
2019-12-29 09:20:56 +1000

    ok, plan B

--- python/update_pijs.py
+++ python/update_pijs.py
@@ -1,18 +1,19 @@
 import json
 from datetime import datetime
 from time import mktime
-from envirophat import light, motion, weather, leds
-from picamera import PiCamera, Color
+from envirophat import light, weather, leds
+
+# from picamera import PiCamera, Color
 
 dt = datetime.now()
 sec_since_epoch = mktime(dt.timetuple()) + dt.microsecond/1000000.0
 unix_epoch = sec_since_epoch * 1000
 
-camera = PiCamera()
-camera.resolution = (800, 450)
-camera.annotate_text = str(round(unix_epoch/1000))
-camera.annotate_text_size = 20
-camera.capture('./node/build/jpg/current-photo.jpg')
+# camera = PiCamera()
+# camera.resolution = (800, 450)
+# camera.annotate_text = str(round(unix_epoch/1000))
+# camera.annotate_text_size = 20
+# camera.capture('./node/build/jpg/current-photo.jpg')
 
 data = {}
 
@@ -32,6 +33,5 @@ data['envirophat'] = envirophatData
 
 with open('./node/build/data/pijs.json', 'w') as outfile:
     json.dump(data, outfile)
-
 
 print("update success https://pi.listingslab.io/data/pijs.json")
