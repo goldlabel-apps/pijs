bdcd9a7ad6533282d284f2cc1ca9a9aed3609281
listingslab <listingslab@gmail.com>
2019-12-28 17:11:49 +1000

    import json

--- python/print_whatevs.py
+++
@@ -1,2 +0,0 @@
-import datetime
-print(datetime.datetime.now().isoformat())
--- python/save_json.py
+++ python/save_json.py
@@ -1,4 +1,16 @@
 # Save JSON
 
+import json
 import datetime
-print(datetime.datetime.now().isoformat())
+
+// print(datetime.datetime.now().isoformat())
+data = {}
+data['people'] = []
+data['people'].append({
+    'name': 'Scott',
+    'website': 'stackabuse.com',
+    'from': 'Nebraska'
+})
+
+with open('../node/build/data/envirophat.json', 'w') as outfile:
+    json.dump(data, outfile)
