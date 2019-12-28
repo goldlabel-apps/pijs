04430af59c03bdac2dd3bddbd6d3ecf7dda9304b
listingslab <listingslab@gmail.com>
2019-12-29 06:11:37 +1000

    catch & release

--- python/update_pijs.py
+++ python/update_pijs.py
@@ -8,13 +8,18 @@ sec_since_epoch = mktime(dt.timetuple()) + dt.microsecond/1000000.0
 unix_epoch = sec_since_epoch * 1000
 
 data = {}
-data['time'] = round(unix_epoch)
-data['lux'] = light.light()
+data['updated'] = round(unix_epoch)
+
+envirophatData = {}
+
+envirophatData['lux'] = light.light()
 leds.on()
-data['rgb'] = str(light.rgb())[1:-1].replace(' ', '')
+envirophatData['rgb'] = str(light.rgb())[1:-1].replace(' ', '')
 leds.off()
-data['temperature'] = weather.temperature()
-data['pressure'] = weather.pressure(unit='hPa')
+envirophatData['temperature'] = weather.temperature()
+envirophatData['pressure'] = weather.pressure(unit='hPa')
+
+data['envirophat'] = envirophatData
 
 with open('./node/build/data/pijs.json', 'w') as outfile:
     json.dump(data, outfile)
