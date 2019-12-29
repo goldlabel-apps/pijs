fd836e40d0fa4fca2640031be8a7a63f11a5de44
deutscher-chris <christoph.dorward.1977@gmail.com>
2019-12-29 12:47:38 +1000

    node

---
+++ node/build/precache-manifest.2b543a5a9e45ce3e93238113b3b28cc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6138ab1f1407d171e524ea61e0296adf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb295c5d8bbd247719bd",
+    "url": "/static/css/main.e3daf8d4.chunk.css"
+  },
+  {
+    "revision": "35f181b02a1aeb58ef4b",
+    "url": "/static/js/2.6cc864d2.chunk.js"
+  },
+  {
+    "revision": "fb295c5d8bbd247719bd",
+    "url": "/static/js/main.4358bb7c.chunk.js"
+  },
+  {
+    "revision": "2fecff885dd2d524e0ab",
+    "url": "/static/js/runtime-main.1c42ca86.js"
+  }
+]);
