37857faadbed80bf90f77cbb3ed8aa00fc9d79d2
listingslab <listingslab@gmail.com>
2020-01-03 21:12:48 +1000

    Gruene Waelde

--- firebase/src/App.jsx
+++ firebase/src/App.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import './App.css';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+};
+firebase.initializeApp(config);
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+  ]
+};
 
 function App() {
+  
+  // console.log()
   return (
     <div className="App">
-      <header className="App-header">
-        PiJS Admin
-      </header>
+      <div className="appShell">
+          <StyledFirebaseAuth
+            uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        </div>
     </div>
   );
 }
