5e9dda13a8b351bf7a1a0eea1ae019770100e278
listingslab <listingslab@gmail.com>
2020-01-03 20:46:39 +1000

    add environment variables

--- firebase/src/index.js
+++ firebase/src/index.js
@@ -1,12 +1,13 @@
+import packageJSON from "../package.json";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+console.log(
+    `${packageJSON.name} ${packageJSON.version} (${process.env.REACT_APP_ENV})`
+);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
