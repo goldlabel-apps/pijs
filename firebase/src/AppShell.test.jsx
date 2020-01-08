a18d199d306bd51ad78ef46c16e4d496da93dabe
listingslab <listingslab@gmail.com>
2020-01-08 16:59:46 +1000

    Move app to firebase

--- firebase/src/AppShell.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import AppShell from './AppShell';
-
-test('contains app name', () => {
-  const { getByText } = render(<AppShell />);
-  const linkElement = getByText(/PiJS/i);
-  expect(linkElement).toBeInTheDocument();
-});
