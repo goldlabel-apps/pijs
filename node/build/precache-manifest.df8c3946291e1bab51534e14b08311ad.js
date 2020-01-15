c24b239a54d8ffe60f8089f437f119e066618c53
listingslab <listingslab@gmail.com>
2020-01-16 06:08:29 +1000

    Final use of the git build workflow
    keeping the comiled code in the repo doesn't work because it exposes the api keys

---
+++ node/build/precache-manifest.df8c3946291e1bab51534e14b08311ad.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086ed2e833705d1fb91dcaf52a9a465b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8c4848bc200fa7f356a",
+    "url": "/static/js/2.6428e351.chunk.js"
+  },
+  {
+    "revision": "2504b9fa89e1513f93e3",
+    "url": "/static/js/main.e31ed739.chunk.js"
+  },
+  {
+    "revision": "8b28100c1706a5ba1b40",
+    "url": "/static/js/runtime-main.7d53a9f3.js"
+  }
+]);
