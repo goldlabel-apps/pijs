eba08db68c8f08bc4a1db5f218c1cba166b6de2b
listingslab <listingslab@gmail.com>
2020-01-16 07:41:23 +1000

    strip

--- react/src/redux/camera/actions.js
+++
@@ -1,9 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-export const update = createAction(`CAMERA/UPDATE`);
-export const broken = createAction(`CAMERA/BROKEN`);
-export const open = createAction(`CAMERA/OPEN`);
-export const close = createAction(`CAMERA/CLOSE`);
-export const toggle = createAction(`CAMERA/TOGGLE`);
-export const error = createAction(`CAMERA/ERROR`);
-export const reset = createAction(`CAMERA/RESET`);
