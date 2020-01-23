743d3f669d3a12347441a3812ce1e7dadc5b6b06
noobie2019 <noobie2019@listingslab.com>
2020-01-24 06:48:06 +1000

    Add photo effect options and varying resolutions

--- bin/create_current_photo.sh
+++ bin/create_current_photo.sh
@@ -6,13 +6,39 @@
 # md 800x450, 
 # lg 1600x900
 
-# for a laugh, let's try some effects
-# --imxfx  negative
-
-raspistill  --imxfx negative --drc high -vf -hf -v -w 400 -h 225 -q 75 -o ~/Desktop/node/pi-firmware/node/current-photos/current-photo_sm_negative.jpg
-
 raspistill  --imxfx none --drc high -vf -hf -v -w 400 -h 225 -q 75 -o ~/Desktop/node/pi-firmware/node/current-photos/current-photo_sm.jpg
 
 raspistill  --imxfx none --drc high -vf -hf -v -w 800 -h 450 -q 450 -o ~/Desktop/node/pi-firmware/node/current-photos/current-photo_md.jpg
 
 raspistill  --imxfx none --drc high -vf -hf -v -w 1600 -h 900 -q 450 -o ~/Desktop/node/pi-firmware/node/current-photos/current-photo_lg.jpg
+
+# for a laugh, let's try some effects
+
+raspistill  --imxfx solarise --drc high -vf -hf -v -w 400 -h 225 -q 75 -o ~/Desktop/node/pi-firmware/node/current-photos/current-photo_sm_solarise.jpg
+
+raspistill  --imxfx film --drc high -vf -hf -v -w 400 -h 225 -q 75 -o ~/Desktop/node/pi-firmware/node/current-photos/current-photo_sm_film.jpg
+
+# --imxfx  negative
+
+# none: no effect (default)
+# negative: invert the image colours
+# solarise: solarise the image
+# posterise: posterise the image
+# whiteboard: whiteboard effect
+# blackboard: blackboard effect
+# sketch: sketch effect
+# denoise: denoise the image
+# emboss: emboss the image
+# oilpaint: oil paint effect
+# hatch: hatch sketch effect
+# gpen: graphite sketch effect
+# pastel: pastel effect
+# watercolour: watercolour effect
+# film: film grain effect
+# blur: blur the image
+# saturation: colour saturate the image
+# colourswap: not fully implemented
+# washedout: not fully implemented
+# colourpoint: not fully implemented
+# colourbalance: not fully implemented
+# cartoon: not fully implemented
