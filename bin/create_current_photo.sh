ffd97b9c6fa1f78b0b3ad902db89a568b15e95fd
noobie2019 <noobie2019@listingslab.com>
2020-01-24 05:24:41 +1000

    Optimises the size of the current photo

--- bin/create_current_photo.sh
+++ bin/create_current_photo.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Optimised image resolution has h/w ratio = 0.5625
 
-# -ae 64,0xff,0x808000 -a "Pi Node Server"
-# --drc high
+# sm 400x225
+# md 800x450, 
+# lg 1600x900
+
+# for a laugh, let's try some effects
 # --imxfx  negative
 
-raspistill  --imxfx none --drc high -vf -hf -v -w 1600 -h 900 -q 70 -o ~/Desktop/node/pi-firmware/node/current-photo.jpg
+raspistill  --imxfx negative --drc high -vf -hf -v -w 400 -h 225 -q 75 -o ~/Desktop/node/pi-firmware/node/current-photo_sm.jpg
+
+raspistill  --imxfx none --drc high -vf -hf -v -w 400 -h 800 -q 450 -o ~/Desktop/node/pi-firmware/node/current-photo_md.jpg
